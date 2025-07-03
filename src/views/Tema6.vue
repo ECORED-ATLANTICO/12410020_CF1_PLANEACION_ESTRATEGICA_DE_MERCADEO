35a9a7020f53bb42b899b7ec69101c844c0cc04e
Fabio Fonseca <fabiofonseca.arg@gmail.com>
2025-07-02 22:40:12 -0500

    tema 6

---
+++ src/views/Tema6.vue
@@ -0,0 +1,131 @@
+<template lang="pug">
+.curso-main-container.pb-3
+  BannerInterno
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
+    .titulo-principal.color-acento-contenido
+      .titulo-principal__numero
+        span 6
+      h1 Análisis situacional
+
+    .row.bg-color-secundario-s10.align-items-center.justify-content-center.p-4.p-md-2.p-xl-0
+      .col-10.col-sm-8.col-lg-3
+        figure
+          img.mb-4.mb-lg-0(data-aos="fade-up")(src="@/assets/curso/temas/tema6/1.png" alt="Imagen decorativa")
+      .col-lg-9
+        p.mb-4.mb-lg-0 Para formular estrategias efectivas en un entorno competitivo y cambiante, las organizaciones necesitan partir de un diagnóstico claro de su realidad. Es aquí donde el análisis situacional cobra relevancia, al permitir identificar los factores internos y externos que influyen en su desempeño. Esta herramienta se convierte en la base para tomar decisiones informadas, adaptarse al contexto y orientar los esfuerzos hacia objetivos alcanzables y sostenibles.
+
+    Separador
+
+
+    #t_6_1(data-aos='zoom-in-right').titulo-segundo.color-acento-contenido.z-2
+      h3 6.1	Concepto
+
+    .row.justify-content-center.align-items-center.z-2
+      .col-lg-7
+        p El análisis situacional consiste en un método ordenado que facilita la detección, clasificación y valoración de los elementos que impactan en el funcionamiento de una organización. Este análisis representa un punto de partida fundamental para el diseño de estrategias efectivas, ya que proporciona un diagnóstico claro sobre las condiciones actuales en las que opera la empresa. Según David (2020), entender tanto las capacidades internas como las condiciones externas es esencial para formular propuestas estratégicas realistas, sostenibles y adaptadas a los cambios del entorno.
+        .row.align-items-center.justify-content-center.p-3.bg-color-terciario-t10.rounded-4.mb-4.mb-lg-0
+          .col-lg-2
+            figure
+              img.mb-4.mb-lg-0.w-6.mx-auto(data-aos="fade-up")(src="@/assets/curso/temas/tema6/3.svg" alt="Imagen decorativa")
+          .col-lg-10
+            p.mb-0 Además de su función diagnóstica, el análisis situacional contribuye a reducir la incertidumbre en la toma de decisiones, al ofrecer una visión comprensiva de los recursos disponibles, las limitaciones, las oportunidades emergentes y los posibles riesgos. Esta herramienta es ampliamente utilizada en contextos empresariales, institucionales y sociales, ya que permite anticipar tendencias y definir ventajas competitivas.
+      .col-lg-5
+        figure
+          img.mb-4.mb-lg-0(data-aos="fade-up")(src="@/assets/curso/temas/tema6/2.png" alt="Imagen decorativa")
+
+    Separador
+
+    #t_6_2(data-aos='zoom-in-right').titulo-segundo.color-acento-contenido.z-2
+      h3 6.2	Variables
+    
+    p.mb-4.mb-lg-5 El análisis situacional distingue dos tipos de factores clave: los internos, propios de la organización, y los externos, relacionados con su entorno.
+
+    .fullwidth-bg.fullwidth-bg--3
+    .row.justify-content-center.align-items-stretch.z-2.image-text
+      .col-md-6.col-xl-5.mb-4.mb-md-0
+        .image-text__card.rounded-5
+          .image-text__header
+            figure
+              img.mb-4.mb-lg-0(data-aos="fade-up")(src='@/assets/curso/temas/tema6/4.png', alt='Imagen decorativa')
+          .image-text__body.p-4
+            h6 Dimensión interna
+            p Comprenden elementos clave dentro de la organización, como los recursos financieros, el talento humano, la estructura organizacional, los procesos operativos, la infraestructura y la cultura corporativa.
+            p Ejemplo: la empresa Alpina S.A. realiza un diagnóstico interno para evaluar su eficiencia en distribución logística a nivel nacional. A partir de los hallazgos, identifica puntos críticos en su operación y reorganiza su cadena de suministro, logrando optimizar costos y mejorar su desempeño operativo.
+      .col-md-6.col-xl-5
+        .image-text__card.rounded-5
+          .image-text__header
+            figure
+              img.mb-4.mb-lg-0(data-aos="fade-up")(src='@/assets/curso/temas/tema6/5.png', alt='Imagen decorativa')
+          .image-text__body.p-4
+            h6 Dimensión externa
+            p Incluyen elementos del entorno que afectan a la organización y están fuera de su control directo, como la competencia, los clientes, la legislación vigente, la economía, la tecnología y el contexto sociocultural.
+            p Ejemplo: ProColombia evalúa el impacto de nuevas regulaciones en Estados Unidos para productos agroindustriales. Con esta información, ajusta sus estrategias de promoción y orienta a los productores en el cumplimiento de los requisitos de exportación.
+
+
+    Separador
+
+    #t_6_3(data-aos='zoom-in-right').titulo-segundo.color-acento-contenido.z-2
+      h3 6.3	Metodología
+
+    .row.justify-content-center.align-items-center.z-2.mb-4.mb-lg-5
+      .col-lg-1.d-none.d-lg-block
+        figure
+          img(src="@/assets/curso/temas/tema6/6.svg" alt="Imagen decorativa")
+      .col-lg-11
+        p.mb-0 La realización de un análisis situacional requiere de un enfoque metodológico claro, que permita convertir la información recolectada en conocimiento estratégico. A continuación, se describen las etapas más comunes en este proceso:
+
+    .row.justify-content-center.align-items-center.factores-slider.z-2.rounded-4.p-4
+      .col-sm-8.col-lg-4.d-none.d-xl-block
+        figure
+          img(src='@/assets/curso/temas/tema6/7.png', alt='Imagen decorativa')
+
+      .col-sm-8.col-md-10.col-xl-8.mb-3.mb-lg-0
+        SlyderF(columnas="col-lg-6")
+          .tarjeta.p-4.card-decoration.rounded-4.mx-2.my-3
+            .row.justify-content-center.mb-3
+              .col-5
+                img.mb-2(src='@/assets/curso/temas/tema6/8.svg' alt='Icon decarativo')
+            h5.bg-color-primario.text-white.text-center.p-3.rounded-3 Recolección de datos
+            p Consiste en obtener información a través de diversas fuentes, como documentos internos, diálogos con directivos, cuestionarios aplicados al personal, observación en campo y el estudio de materiales externos como reportes sectoriales y datos estadísticos públicos.
+
+          .tarjeta.p-4.card-decoration.rounded-4.mx-2.my-3
+            .row.justify-content-center.mb-3
+              .col-5
+                img.mb-2(src='@/assets/curso/temas/tema6/9.svg' alt='Icon decarativo')
+            h5.bg-color-primario.text-white.text-center.p-3.rounded-3 Clasificación de variables
+            p Se agrupan los factores en dos categorías principales (internos y externos) para facilitar su evaluación e interpretación.
+
+          .tarjeta.p-4.card-decoration.rounded-4.mx-2.my-3
+            .row.justify-content-center.mb-3
+              .col-5
+                img.mb-2(src='@/assets/curso/temas/tema6/10.svg' alt='Icon decarativo')
+            h5.bg-color-primario.text-white.text-center.p-3.rounded-3 Aplicación de herramientas de diagnóstico
+            p Se emplean matrices como DOFA (Debilidades, Oportunidades, Fortalezas y Amenazas), análisis PESTEL (Político, Económico, Social, Tecnológico, Ecológico y Legal), y otras técnicas como <em>benchmarking</em> o análisis de brechas.
+
+          .tarjeta.p-4.card-decoration.rounded-4.mx-2.my-3
+            .row.justify-content-center.mb-3
+              .col-5
+                img.mb-2(src='@/assets/curso/temas/tema6/11.svg' alt='Icon decarativo')
+            h5.bg-color-primario.text-white.text-center.p-3.rounded-3 Conclusiones estratégicas
+            p Se interpretan los hallazgos para construir un diagnóstico que permita tomar decisiones informadas, formular estrategias competitivas y aprovechar el entorno.
+
+</template>
+
+<script>
+export default {
+  name: 'Tema6',
+  data: () => ({
+    // variables de vue
+  }),
+  mounted() {
+    this.$nextTick(() => {
+      this.$aosRefresh()
+    })
+  },
+  updated() {
+    this.$aosRefresh()
+  },
+}
+</script>
+
+<style lang="sass"></style>
